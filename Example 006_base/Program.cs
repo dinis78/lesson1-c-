ecdbc63ab55cb9fff6d54c05ce68e43a9baaf03c
Denis <denis-shkurko@yandex.ru>
2022-08-05 12:39:40 +0700

    add Example 006_base

---
+++ Example 006_base/Program.cs
@@ -0,0 +1,16 @@
+﻿int a=45;
+int b=8;
+int c=20;
+int d=49;
+int e=78;
+
+int max= a;
+
+if(a>max) max=a;
+if(b>max) max=b;
+if(c>max) max=c;
+if(d>max) max=d;
+if(e>max) max=e;
+
+Console.Write("Max=");
+Console.WriteLine(max);
