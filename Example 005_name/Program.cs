7851a4e37cb3d4a447956231b41fbe1559c7da34
Denis <denis-shkurko@yandex.ru>
2022-08-05 12:28:00 +0700

    add Example 005_name

---
+++ Example 005_name/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите Ваше имя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "света")
+
+{
+
+    Console.WriteLine("Привет Красавитца!!!");
+}
+
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine (username);
+}
